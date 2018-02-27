3a7b1bd04ed3ed951791581cdc711e104a0f2cfc
vang_machine <vangogius@gmail.com>
2018-02-27 18:01:28 +0200

    Upgrade performance for pos tagger

--- birchnlp/pos_tagger/pos_tagger.py
+++ birchnlp/pos_tagger/pos_tagger.py
@@ -10,23 +10,28 @@ import scipy.sparse as sprs
 from birchnlp.pos_tagger.schemes import POSTag
 from birchnlp.utils import get_stem_func
 
-END_TOKEN = "$END$"
-START_TOKEN = "$START$"
 
-FEATURE_SENT_START = "sentence_start"
-FEATURE_SENT_END = "sentence_end"
-WORD_FEATURE_PATTERN = "{}_word_{}"
-BASIC_FEATURE_PATTERN = "{}_basic_{}"
-SUFFIX_FEATURE_PATTERN = "{}_suffix_{}"
-PREFIX_FEATURE_PATTERN = "{}_prefix_{}"
+SEED = 1337
+FEATURES_COUNT = 2**22
+
+def get_hash(feat):
+    feat = feat.encode("utf-8")
+    return xxhash.xxh64(feat, seed=SEED).intdigest() % FEATURES_COUNT
+
+
+FEATURE_SENT_START_HASH = get_hash("sentence_start")
+FEATURE_SENT_END_HASH = get_hash("sentence_end")
+START_TOKEN = "$START$"
+END_TOKEN = "$END$"
+WORD_FEATURE_PATTERN = "%d_word_%s"
+BASIC_FEATURE_PATTERN = "%d_basic_%s"
+SUFFIX_FEATURE_PATTERN = "%d_suffix_%s"
+PREFIX_FEATURE_PATTERN = "%d_prefix_%s"
 
 MIN_CHARS = 2
 MAX_CHARS = 5
 MIN_WORD_LEN = 4
 
-FEATURES_COUNT = 2**22
-
-SEED = 1337
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'weights')
 COEF_FILE = "coef.npz"
@@ -46,38 +51,46 @@ class POSTagger(object):
 
         return coef, intercept
 
-    def _get_features_words_window(self, words_window):
+    def _get_features_words_window(self, words_window: t.List[str]
+                                   ) -> t.List[int]:
+
+        features = set()
+
         if words_window[1] == START_TOKEN:
-            yield FEATURE_SENT_START
+            features.add(FEATURE_SENT_START_HASH)
         elif words_window[3] == END_TOKEN:
-            yield FEATURE_SENT_END
+            features.add(FEATURE_SENT_END_HASH)
 
         for i, word in enumerate(words_window):
             if word in {START_TOKEN, END_TOKEN}:
-                yield WORD_FEATURE_PATTERN.format(i, word)
+                features.add(get_hash(WORD_FEATURE_PATTERN % (i, word)))
                 continue
 
             for ind, feat in enumerate(get_basic_features(word)):
-                yield BASIC_FEATURE_PATTERN.format(i, feat)
+                features.add(get_hash(BASIC_FEATURE_PATTERN % (i, feat)))
 
             word = word.lower()
 
             for c in range(MIN_CHARS, MAX_CHARS):
-                yield SUFFIX_FEATURE_PATTERN.format(i, word[-c:])
-                yield PREFIX_FEATURE_PATTERN.format(i, word[:c])
+                features.add(get_hash(SUFFIX_FEATURE_PATTERN % (i, word[-c:])))
+                features.add(get_hash(PREFIX_FEATURE_PATTERN % (i, word[:c])))
 
             if len(word) >= MIN_WORD_LEN:
                 word = self.stemmer(word)
 
-            yield WORD_FEATURE_PATTERN.format(i, word)
+            features.add(get_hash(WORD_FEATURE_PATTERN % (i, word)))
+
+        return features
 
     def _get_features_sent(self, sent):
         sent_new = [START_TOKEN, START_TOKEN] + sent + [END_TOKEN, END_TOKEN]
 
+        hashes_matrix = []
         for i in range(len(sent_new) - 4):
-            feats_gen = self._get_features_words_window(sent_new[i:i + 5])
-            hashes = set(get_hash(feat) for feat in feats_gen)
-            yield hashes
+            feats_hashes = self._get_features_words_window(sent_new[i:i + 5])
+            hashes_matrix.append(feats_hashes)
+
+        return hashes_matrix
 
     def _get_features_matrix(self, words: t.List[str]):
         indptr, indices = [0], []
@@ -104,14 +117,17 @@ class POSTagger(object):
         return tags
 
 
-def get_hash(feat):
-    feat = feat.encode("utf-8")
-    return xxhash.xxh64(feat, seed=SEED).intdigest() % FEATURES_COUNT
-
-
 def get_basic_features(word):
-    for key, func in FEATURE_PREDICATES.items():
-        yield f"{key}_{func(word)}"
+    features = [
+        "is_upper_%s" % word.isupper(),
+        "is_number_%s" % is_number(word),
+        "is_title_%s" % word.istitle(),
+        "has_score_%s" % ("-" in word),
+        "has_dot_%s" % ("." in word),
+        "word_len_%d" % len(word),
+    ]
+
+    return features
 
 
 def is_number(x):
@@ -120,13 +136,3 @@ def is_number(x):
         return True
     except ValueError:
         return False
-
-
-FEATURE_PREDICATES = {
-    "is_upper": lambda x: x.isupper(),
-    "is_number": is_number,
-    "is_title": lambda x: x.istitle(),
-    "has_score": lambda x: "-" in x,
-    "has_dot": lambda x: "." in x,
-    "word_len": len,
-}
